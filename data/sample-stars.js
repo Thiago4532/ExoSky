76f5f44f5c81fc37983efacb9409c5ad7cd75c19
Pedro Mesquita <p183998@dac.unicamp.br>
2024-10-06 03:11:04 -0300

    add sample stars data

---
+++ data/sample-stars.js
@@ -0,0 +1,44 @@
+export const sampleStars = [
+    {
+        name: "Vega",
+        distance: 7.68,
+        magnitude: 0.582,
+        ra: '18h 36m 56.33635s',
+        dec: '+38° 47′ 01.2802″'
+    },
+    {
+        name: "Acrux",
+        distance: 99,
+        magnitude: -3.77,
+        ra: '12h 26m 35.89522s',
+        dec: '-63° 05′ 56.7343″',
+    },
+    {
+        name: "Mimosa",
+        distance: 85,
+        magnitude: -3.92,
+        ra: '12h 47m 43.26877s',
+        dec: '-59° 41′ 19.5792″',
+    },
+    {
+        name: "Gacrux",
+        distance: 27.2,
+        magnitude: -0.68,
+        ra: '12h 31m 09.960s',
+        dec: '-57° 06′ 47.568″',
+    },
+    {
+        name: "Imai",
+        distance: 106,
+        magnitude: -3.2,
+        ra: '12h 15m 08.71673s',
+        dec: '-58° 44′ 56.1369″',
+    },
+    {
+        name: "Ginan",
+        distance: 70.4,
+        magnitude: -0.63,
+        ra: '12h 21m 21.608s',
+        dec: '-60° 24′ 04.13″',
+    },
+];
